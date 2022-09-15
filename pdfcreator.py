6b3efa64972267b3ab517958127241336a98825d
maxime NGUYEN <ngmaxime@gmail.com>
2022-09-15 22:29:50 +0200

    ajout fichiers compilables standalone

---
+++ pdfcreator.py
@@ -0,0 +1,24 @@
+# coding: utf8
+import os, sys, subprocess
+
+# créer le fichier sources d'une planche, en donnant une liste de numéros à 4 chiffres
+
+def createlatex(directory, filename):
+        texte = '\\documentclass[preview,border=2pt]{standalone} \n \\usepackage[utf8]{inputenc} \n \\usepackage[T1]{fontenc} \n \\usepackage[frenchb]{babel} \n \\newcommand{\path}{} \n \\input{\path_preambules/general.tex} \n \\input{\path_preambules/print.tex} \n \\input{\path_preambules/macros.tex} \n \\input{\path_preambules/entetes.tex} \n \\setboolean{solution}{true} \n'
+        texte += '\n \\begin{document}'
+        texte += '\n \\insertexo{'+str(filename)+'} \n'
+        texte += '\\end{document}'
+        latexfile = open(os.path.join(directory,filename),"w")
+        latexfile.write(texte)
+        latexfile.close()
+
+directory = 'compile/latex'
+directory_src = 'src'
+# createlatex(directory,'AbCd')
+
+for filename in os.listdir(directory_src):
+    f = os.path.join(directory_src, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        if f[-4:] == '.tex':
+            createlatex(directory,filename)
