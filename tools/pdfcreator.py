5a895a7c6849307c68114552098232c47da021ea
smaxx73 <ngmaxime@gmail.com>
2023-09-01 08:56:11 +0200

    modif outil compilation

--- tools/pdfcreator.py
+++ tools/pdfcreator.py
@@ -9,7 +9,7 @@ def createlatex(directory, filename):
     #    texte += '\\usepackage[utf8]{inputenc} \n \\usepackage[T1]{fontenc} \n \\usepackage[frenchb]{babel} \n'
         texte += '\\newcommand{\\path}{../../} \n \\input{\\path_preambules/general.tex} \n \\input{\\path_preambules/print.tex} \n \\input{\\path_preambules/macros.tex} \n \\setboolean{solution}{true} \n'
         texte += '\n \\begin{document}'
-        texte += '\n \\insertexo{'+str(filename[:len(filename)-4])+'}{true}{true}{false}{} \n'
+        texte += '\n \\insertexobis{'+str(filename[:len(filename)-4])+'}{true}{true}{false}{} \n'
         texte += '\\end{document}'
         latexfile = open(os.path.join(directory,filename),"w")
         latexfile.write(texte)
@@ -18,35 +18,55 @@ def createlatex(directory, filename):
 script_directory = os.path.dirname(os.path.abspath(__file__))
 parent_directory = os.path.join(script_directory, os.pardir)
 
-directory_compile = os.path.join(parent_directory, 'pdf/latex') 
-directory_src = os.path.join(parent_directory, 'src')  
-directory_pdf = os.path.join(parent_directory, 'pdf/pdf') 
+def compile_latex(file_path, output_directory):
+    os.system('xelatex ' + '--shell-escape -synctex=0 -interaction=nonstopmode -output-directory=' + output_directory + ' ' + file_path)
 
-print(script_directory)
-print(directory_pdf)
-print(directory_src)
-print(directory_compile)
+def should_compile(file_src, file_pdf):
+    """Determine if a file should be compiled based on its last modification time."""
+    if not os.path.isfile(file_pdf):
+        return True
 
-
-for filename in os.listdir(directory_src):
-    if filename[-4:] == ".tex":
-        filename = filename[:-4]
-    file_pdf = os.path.join(directory_pdf, filename)+'.pdf'
-    file_src = os.path.join(directory_src, filename)+'.tex'
-    file_compile = os.path.join(directory_compile, filename)+'.tex'
-    # checking if it is a file
-    if os.path.isfile(file_compile) == 0:
-        createlatex(directory_compile,filename+'.tex')
-        print(filename+'.tex is created in compilable folder')
-    if os.path.isfile(file_pdf) == 0:
-        print('compiling...')
-        os.system('xelatex ' + ' -synctex=1 -interaction=nonstopmode -output-directory=' + directory_pdf + ' ' + os.path.join(directory_compile, filename+'.tex'))
-        print('compiling ended')
     time_pdf = os.path.getmtime(file_pdf)
     time_src = os.path.getmtime(file_src)
-    if time_src > time_pdf:
-        print(filename +' has been updated but not compiled')
-        print('compiling...')
-        os.system('xelatex ' + '--shell-escape -synctex=1 -interaction=nonstopmode -output-directory=' + directory_pdf + ' ' + os.path.join(directory_compile, filename+'.tex'))
-        print('compiling ended')
 
+    return time_src > time_pdf
+
+def main(directory_src, directory_pdf, directory_compile):
+    for filename in os.listdir(directory_src):
+        if filename.endswith(".tex"):
+            base_filename = filename[:-4]
+            file_pdf = os.path.join(directory_pdf, base_filename + '.pdf')
+            file_src = os.path.join(directory_src, base_filename + '.tex')
+            file_compile = os.path.join(directory_compile, base_filename + '.tex')
+
+            # checking if it is a file
+            if not os.path.isfile(file_compile):
+                createlatex(directory_compile, base_filename + '.tex')
+                print(base_filename + '.tex is created in compilable folder')
+
+            if should_compile(file_src, file_pdf):
+                print(base_filename + ' has been updated or not compiled')
+                print('compiling...')
+                compile_latex(file_compile, directory_pdf)
+                print('compiling ended')
+
+if __name__ == "__main__":
+    # Remplacez ces chemins par les chemins appropriés
+    directory_compile = os.path.join(parent_directory, 'pdf/latex') 
+    directory_src = os.path.join(parent_directory, 'src')  
+    directory_pdf = os.path.join(parent_directory, 'pdf/pdf') 
+    
+    # test sur un seul fichier
+    filename = 'kJz6.tex'
+    createlatex(directory_compile, filename)
+    print(os.path.join(directory_compile, filename))
+    print(directory_pdf)
+    # compile_latex(os.path.join(directory_compile, filename), directory_pdf)
+    # os.system("xelatex --shell-escape -synctex=0 -interaction=nonstopmode -output-directory=/home/maxime/Documents/Exercices/tools/../pdf/pdf /home/maxime/Documents/Exercices/tools/../pdf/latex/kJz6.tex")
+    # main(directory_src, directory_pdf, directory_compile)
+
+
+def compile_latex_file(filename):
+    subprocess.call(["xelatex", "--shell-escape", "-synctex=0", "-interaction=nonstopmode", "-output-directory=/home/maxime/Documents/Exercices/tools/../pdf/pdf", filename])
+
+compile_latex_file("/home/maxime/Documents/Exercices/tools/../pdf/latex/kJz6.tex")
