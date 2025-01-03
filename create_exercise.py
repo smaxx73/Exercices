bafb89feee3eb412c4097d9c97911e97c4d56884
smaxx73 <ngmaxime@gmail.com>
2025-01-03 15:34:28 +0100

    modif script creation exercice

--- create_exercise.py
+++ create_exercise.py
@@ -2,6 +2,7 @@ import os
 import random
 import string
 import subprocess
+from datetime import date  # <-- ajouté pour gérer la date du jour
 
 def generate_unique_id(existing_ids):
     while True:
@@ -25,17 +26,24 @@ def create_exercise_file(template_path, output_directory, unique_id):
     # Lit le template LaTeX
     with open(template_path, 'r') as template_file:
         content = template_file.read()
+
     # Remplace les espaces réservés par les valeurs réelles
     content = content.replace('{ID}', unique_id)
-    # Vous pouvez ajouter plus de remplacements ici si nécessaire
+    
+    # Récupère la date du jour et remplace {AAAA-MM-DD} dans le template
+    current_date = date.today().strftime("%Y-%m-%d")
+    content = content.replace('{YYYY-MM-DD}', current_date)
+    
     # Par exemple, demander à l'utilisateur d'entrer l'énoncé de l'exercice
     enonce = input("Entrez le titre de l'exercice:\n")
     content = content.replace('{TITRE}', enonce)
+
     # Enregistre le nouveau fichier LaTeX
     output_filename = f"{unique_id}.tex"
     output_path = os.path.join(output_directory, output_filename)
     with open(output_path, 'w') as output_file:
         output_file.write(content)
+
     print(f"Fichier créé : {output_path}")
     return output_path
 
@@ -48,15 +56,20 @@ def main():
     # Définissez les chemins appropriés
     exercises_directory = './src'
     template_path = './template_exercice.tex'
+
     # Vérifie que le répertoire des exercices existe
     if not os.path.exists(exercises_directory):
         os.makedirs(exercises_directory)
+
     # Récupère les identifiants existants
     existing_ids = get_existing_ids(exercises_directory)
+
     # Génère un identifiant unique
     unique_id = generate_unique_id(existing_ids)
+
     # Crée le fichier de l'exercice
     output_path = create_exercise_file(template_path, exercises_directory, unique_id)
+
     # Ajoute le fichier au dépôt git et effectue un commit
     git_add_and_commit(output_path, f'Ajout de l\'exercice {unique_id}')
 
